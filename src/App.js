31c6d5f26bf779aabd3c6997e6e12c206f70a7dc
Noah <85143444+npstrass@users.noreply.github.com>
2021-08-02 23:39:30 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import SearchMovies from "./SearchMovies";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovies />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SearchMovies.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import MovieCard from "./MovieCard";
+
+function SearchMovies() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=bf3509658cf02947aac01e32e2950350&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <input
+          className="input"
+          aria-label="movie search query"
+          type="text"
+          name="query"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="ie Remember the Titans"
+        />
+        <button className="btn" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
+}
+
+export default SearchMovies;
+
+//to create the SearchMovies component
+//form with a class of form
+//label with htmlFor="query" and a class of Label
+//input of type text with a name of "query" and a placeholder
+//button class of button and a type of submit
